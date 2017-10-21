04f3050133fc9499685fbdddc87691b5b64665cf
djimon <Djimon@gmx.de>
2017-10-21 16:41:30 +0200

    fixed animations

--- GameJamInfinity/Assets/Scripts/Player.cs
+++ GameJamInfinity/Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
 
     private Rigidbody2D rb;
     private Vector2 Velocity = new Vector2(0, 0);
+    private bool isWalking = false;
 
     private Animator anim;
 	// Use this for initialization
@@ -22,7 +23,8 @@ public class Player : MonoBehaviour {
 
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         movement();
         animationupdate();
 	}
@@ -36,30 +38,45 @@ public class Player : MonoBehaviour {
 
     void movement()
     {
+        Debug.Log(Velocity);
         Velocity = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.A))
         {
             Velocity += new Vector2(-1, 0);
+            isWalking = true;
+            Debug.Log("is walking");
             //transform.position += new Vector3 (-1*speed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             Velocity += new Vector2(1, 0);
+            isWalking = true;
+            Debug.Log("is walking");
             //transform.position += new Vector3(1*speed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             Velocity += new Vector2(0, -1);
+            isWalking = true;
+            Debug.Log("is walking");
             //transform.position += new Vector3(0,-1*speed, 0);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             Velocity += new Vector2(0, 1);
+            isWalking = true;
+            Debug.Log("is walking");
             //transform.position += new Vector3(0, 1*speed, 0);
+        }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            isWalking = false;
+            Debug.Log("!walking");
         }
 
         transform.position += speed * new Vector3(Velocity.x,Velocity.y);
